b24d638f8b3d238bcbe66d77825eb1359fcf8a7a
Sebastian <77078896+SlowlyOwly@users.noreply.github.com>
2022-08-12 15:10:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import sklearn
+
+
+# Web App schema
+st.write("# 10 Year Heart Disease Prediction")
+
+col1, col2, col3 = st.columns(3)
+
+gender = col1.selectbox("Enter your gender", ["Female", "Male"])
+
+age = col2.number_input("Enter your age")
+
+education = col3.selectbox("Highest academic qualification",
+                           ["High school diploma", "Undergraduate degree", "Postgraduate degree", "PhD"])
+
+isSmoker = col1.selectbox("Are you currently a smoker?", ["Yes", "No"])
+
+yearsSmoking = col2.number_input("Number of daily cigarettes")
+
+BPMeds = col3.selectbox("Are you currently on BP medication?", ["Yes", "No"])
+
+stroke = col1.selectbox("Have you ever experienced a stroke?", ["Yes", "No"])
+
+hyp = col2.selectbox("Do you have hypertension?", ["Yes", "No"])
+
+diabetes = col3.selectbox("Do you have diabetes?", ["Yes", "No"])
+
+chol = col1.number_input("Enter your cholesterol level")
+
+sys_bp = col2.number_input("Enter your systolic blood pressure")
+
+dia_bp = col3.number_input("Enter your diastolic blood pressure")
+
+bmi = col1.number_input("Enter your BMI")
+
+heart_rate = col2.number_input("Enter your resting heart rate")
+
+glucose = col3.number_input("Enter your glucose level")
+
+
+# Data from survey transformation for prediction
+df_pred = pd.DataFrame([[gender, age, education, isSmoker, yearsSmoking, BPMeds, stroke, hyp, diabetes, chol, sys_bp,
+                         dia_bp, bmi, heart_rate, glucose]],
+                       columns=['gender', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds',
+                                'prevalentStroke', 'prevalentHyp',
+                                'diabetes', 'totChol', 'sysBP', 'diaBP', 'BMI', 'heartRate', 'glucose'])
+
+df_pred['gender'] = df_pred['gender'].apply(lambda x: 1 if x == "Male" else 0)
+
+df_pred['prevalentHyp'] = df_pred['prevalentHyp'].apply(lambda x: 1 if x == "Yes" else 0)
+
+df_pred['prevalentStroke'] = df_pred['prevalentStroke'].apply(lambda x: 1 if x == "Yes" else 0)
+
+df_pred['diabetes'] = df_pred['diabetes'].apply(lambda x: 1 if x == "Yes" else 0)
+
+df_pred['BPMeds'] = df_pred['BPMeds'].apply(lambda x: 1 if x == "Yes" else 0)
+
+df_pred['currentSmoker'] = df_pred['currentSmoker'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+
+def transform(data):
+    result = 3
+    if data == 'High school diploma':
+        result = 0
+    elif data == 'Undergraduate deegree':
+        result = 1
+    elif data == 'Postgraduate deegree':
+        result = 2
+    return result
+
+
+df_pred['education'] = df_pred['education'].apply(transform)
+
+# ML model loading
+model = joblib.load('fhs_rf_model.pkl')
+prediction = model.predict(df_pred)
+
+# Prediction display
+if st.button("Predict"):
+
+    if prediction[0] == 0:
+        st.write('You likely will not develop heart disease in 10 years.')
+    else:
+        st.write('You are likely to develop heart disease in 10 years.')
+
+    print(prediction)
